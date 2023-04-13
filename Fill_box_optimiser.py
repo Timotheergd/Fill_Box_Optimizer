db9378122179493173c7bb984a24fb728ee5c2e7
timothee <timothee@debian-BULLSEYE-live-builder-AMD64>
2023-04-13 20:25:53 +0200

    first changes to translate in english v2 but still not fully translated

--- Fill_box_optimiser.py
+++ Fill_box_optimiser.py
@@ -1,8 +1,7 @@
-def printf_clear(log_file, debug=True):
+def printf_clear(log_file):
   # Clear the log file
   with open(log_file, 'w') as f:
-    if debug:
-      f.write('Hi, this is the log file.\n')
+    f.write('Hi, this is the log file.\n')
 
 def printf(log_file, *text):
   # Write the text in the log file
@@ -69,7 +68,7 @@ class Pallet(Box):
         self.content.append([rotation, nb_L, nb_w, nb_h])
         self.nb_box += nb_L * nb_w * nb_h
 
-  def remove_box(self, rotation, nb_L, nb_w, nb_h, log_file=".log"):
+  def remove_box(self, rotation, nb_L, nb_w, nb_h, debug=False, log_file=".log"):
     """
     Remove boxes from the pallet from the rotation and the number of boxes in the lenght, width and height
     """
@@ -79,8 +78,9 @@ class Pallet(Box):
       self.content.remove([rotation, nb_L, nb_w, nb_h])
       self.nb_box -= nb_L * nb_w * nb_h
     except:
-      printf(log_file, "failed to remove boites from the pallet"+"*"*10)
-      printf(log_file, f"{self.content=}")
+      if debug:
+        printf(log_file, "failed to remove boites from the pallet"+"*"*10)
+        printf(log_file, f"{self.content=}")
 
   def __gt__(self, other):
     return self.nb_box > other.nb_box
@@ -98,14 +98,14 @@ class Pallet(Box):
     for i in range(len(other.content)):
       self.add_box(other.content[i][0], other.content[i][1], other.content[i][2], other.content[i][3])
 
-  def print_way_fill(self, print_to_console=False):
+  def print_way_fill(self, debug=False):
     """
     Explain how you need to rotate the boxes in the pallet
     """
     text_to_print=""
     nb_step = 1
     text_to_print+=f"Way to fill the pallet :\n{nb_step}. Position the pallet with the long side facing you.\n"
-    if print_to_console: print(f"Way to fill the pallet :\n{nb_step}. Position the pallet with the long side facing you.")
+    if debug: print(f"Way to fill the pallet :\n{nb_step}. Position the pallet with the long side facing you.")
     for step in self.content:
       nb_step+=1
       rotation_text=""
@@ -122,16 +122,16 @@ class Pallet(Box):
       elif step[0]==6:
           rotation_text="vertically on the short edge, parallel to the width of the pallet"
       
-      if print_to_console: print(f"{nb_step}. Position (L*w*h) {step[1]}*{step[2]}*{step[3]} = {step[1]*step[2]*step[3]} box(es) " + rotation_text)
+      if debug: print(f"{nb_step}. Position (L*w*h) {step[1]}*{step[2]}*{step[3]} = {step[1]*step[2]*step[3]} box(es) " + rotation_text)
       text_to_print+=f"{nb_step}. Position {step[1]*step[2]*step[3]} box(es) " + rotation_text + "\n"
     
     return text_to_print
 
-  def print_total(self, print_to_console=False):
+  def print_total(self, debug=False):
     """
     Return text : how many boxes are positioned in the pallet ?, with the number in an other variable, to print it in bold later
     """
-    if print_to_console:
+    if debug:
       print(f"You have now placed {self.nb_box} box(es) in the pallet")
     text_to_print_start=f"You have now placed"
     text_to_print_bold=f" {self.nb_box} "
@@ -139,7 +139,6 @@ class Pallet(Box):
     return (text_to_print_start, text_to_print_bold, text_to_print_end)
 
 def possibilities(dimensions):
-  # renvoie lensemble des arrangements possibles de la liste (3d)
   """
   Return all arragement of the list
   Each arrangement is associated with a number, this correspond to the rotation
@@ -151,7 +150,6 @@ def possibilities(dimensions):
   [h, w, L] -> 6
   """
   p = []
-
   for i in range(3):
     k = [0, 1, 2]
     n1 = k[i]
@@ -161,88 +159,87 @@ def possibilities(dimensions):
       n3 = k[(j + 1) % 2]
       a = [dimensions[n1], dimensions[n2], dimensions[n3]]
       p.append(a)
-
   return p
-
-
 
 def fill_pallet(_pallet,
                 box,
                 fill_rest=True,
                 _previous_pallet=Pallet(),
-                _print=False):
+                debug=False,
+                log_file=".log"):
   best_pallet = Pallet()               
   
-  n = 0  # pour savoir le nombre d'itération de dim
+  n = 0  # to get track of the current dimension
   for dim in possibilities(box.get_dim()):
-    carton = _pallet.copy()
-    cartons_reste = []
-    carton_precedent = _previous_pallet.copy()  
+    pallet = _pallet.copy()
+    pallet_fill_rest = []
+    previous_pallet = _previous_pallet.copy()  
 
-    if _print:
+    if debug:
       if (fill_rest): printf(log_file, "\n" * 2 + "-+*+-" * 10 + "________base")
-      else: printf(log_file, "________reste")
-      printf(log_file, dim, " in ", carton.get_dim())
+      else: printf(log_file, "________rest")
+      printf(log_file, dim, " in ", pallet.get_dim())
 
     n += 1
-    if _print: printf(log_file, "n=", n)
+    if debug: printf(log_file, "n=", n)
 
-    nb_L = carton.L // dim[0]
+    # Calculate the number of boxes we can fit on the lenght(l), width(w), height(h)
+    nb_L = pallet.L // dim[0]
     L = dim[0] * nb_L
 
-    nb_w = carton.w // dim[1]
+    nb_w = pallet.w // dim[1]
     w = dim[1] * nb_w
 
-    nb_h = carton.h // dim[2]
+    nb_h = pallet.h // dim[2]
     h = dim[2] * nb_h
 
+    # Calculate the space left on the sides
     L_r, w_r, h_r = 0, 0, 0
     if not nb_L*nb_w*nb_h == 0:
-      L_r = carton.L - L
-      w_r = carton.w - w
-      h_r = carton.h - h
+      L_r = pallet.L - L
+      w_r = pallet.w - w
+      h_r = pallet.h - h
 
-    carton.add_box(n, nb_L, nb_w, nb_h)
+    pallet.add_box(n, nb_L, nb_w, nb_h)
 
-    carton_precedent.combine(carton)
+    previous_pallet.combine(pallet)
 
-    if _print: 
-      printf(log_file, "nb presentoirs=", nb_L, "x", nb_w, "x", nb_h, " =", nb_L * nb_w * nb_h)
-      printf(log_file, "nb presentores du carton=", nb_L, "x", nb_w, "x", nb_h, " =", carton.nb_box)
-      printf(log_file, "total=", carton_precedent.nb_box)
-      printf(log_file, "total=", carton_precedent.content)
+    if debug: 
+      printf(log_file, "nb box=", nb_L, "x", nb_w, "x", nb_h, " =", nb_L * nb_w * nb_h)
+      printf(log_file, "nb box in pallet=", nb_L, "x", nb_w, "x", nb_h, " =", pallet.nb_box)
+      printf(log_file, "total=", previous_pallet.nb_box)
+      printf(log_file, "total=", previous_pallet.content)
       printf(log_file, "total=", L, "x", w, "x", h)
-      printf(log_file, "reste=", L_r, "x", w_r, "x", h_r)
+      printf(log_file, "rest=", L_r, "x", w_r, "x", h_r)
 
-    # CHECK IF IT IS THE BEST CARTON
-    if carton_precedent > best_pallet:
-      best_pallet = carton_precedent.copy()
+    # Check if the pallet is better than the previous
+    if previous_pallet > best_pallet:
+      best_pallet = previous_pallet.copy()
 
-    # Fill the gaps
+    # Fill the gaps by creating new virtual pallets
     if (fill_rest):
-      carton_L = Pallet(L_r, carton.w, carton.h)
-      carton_l = Pallet(carton.L, w_r, carton.h)
-      carton_h = Pallet(carton.L, carton.w, h_r)
-      cartons_reste.append(carton_L)
-      cartons_reste.append(carton_l)
-      cartons_reste.append(carton_h)
-      if _print: 
+      pallet_L = Pallet(L_r, pallet.w, pallet.h)
+      pallet_l = Pallet(pallet.L, w_r, pallet.h)
+      pallet_h = Pallet(pallet.L, pallet.w, h_r)
+      pallet_fill_rest.append(pallet_L)
+      pallet_fill_rest.append(pallet_l)
+      pallet_fill_rest.append(pallet_h)
+      if debug: 
         printf(log_file, "="*10 + "\nPallets to fill \n" + "="*10)
-        for c in cartons_reste:
+        for c in pallet_fill_rest:
           printf(log_file, c)
-      for carton in cartons_reste:
-        best_little_carton_fill = fill_pallet(carton, box, False, carton_precedent.copy(), _print)
-        carton_precedent.combine(best_little_carton_fill)
+      for pallet in pallet_fill_rest:
+        best_little_pallet_fill = fill_pallet(pallet, box, False, previous_pallet.copy(), debug, log_file)
+        previous_pallet.combine(best_little_pallet_fill)
 
-        if carton_precedent > best_pallet:
-          best_pallet = carton_precedent.copy()
+        if previous_pallet > best_pallet:
+          best_pallet = previous_pallet.copy()
 
-    carton.remove_box(n, nb_L, nb_w, nb_h)
+    pallet.remove_box(n, nb_L, nb_w, nb_h, log_file, debug)
 
-  if _print: 
+  if debug: 
     printf(log_file, "\n\n\n")
     printf(log_file, "*" * 20)
-    # printf(log_file, "nb_cartons=", nb_cartons_total)
     printf(log_file, "best pallet=", best_pallet.nb_box)
     printf(log_file, "content=", best_pallet.content)
     printf(log_file, "*" * 20)
@@ -250,8 +247,10 @@ def fill_pallet(_pallet,
 
 import tkinter as tk
 
-def validate_sizes():
-    
+def validate_sizes(debug=False):
+    """
+    Update the ui after confirm to explain how to fill the pallet
+    """
     global big_box_width
     global big_box_height
     global big_box_depth
@@ -273,22 +272,22 @@ def validate_sizes():
     big_box_size = f"{big_box_width} x {big_box_height} x {big_box_depth}"
     small_box_size = f"{small_box_width} x {small_box_height} x {small_box_depth}"
 
-    carton = Pallet(int(big_box_width), int(big_box_depth), int(big_box_height))
+    pallet = Pallet(int(big_box_width), int(big_box_depth), int(big_box_height))
     box = Box(int(small_box_width), int(small_box_depth), int(small_box_height))
-    best_pallet = fill_pallet(carton, box)
+    best_pallet = fill_pallet(pallet, box, debug, log_file)
 
     global root
     
-    root.title("Resultat")
+    root.title("Result")
     root.geometry("1000x500")
     global result_label
     
     result_label.configure(state="normal")
     result_label.delete("1.0",tk.END)
-    result_label.insert("1.0", f"Taille du carton: {big_box_size}\nTaille du présentoir: {small_box_size}\n\n" + str(best_pallet.print_way_fill()))
+    result_label.insert("1.0", f"Size of the pallet: {big_box_size}\nSize of the boxes: {small_box_size}\n\n" + str(best_pallet.print_way_fill()))
     end_pos = int(result_label.index("end").split(".")[0])
 
-    text_start, text_bold, text_end = best_pallet.print_total()
+    text_start, text_bold, text_end = best_pallet.print_total(debug)
     
     result_label.insert("end", text_start, ('normal'))
     result_label.insert("end", text_bold, "Arial bold 12")
@@ -302,17 +301,19 @@ def validate_sizes():
     result_label.configure(state="disabled")
     result_label.pack(fill="both")
     
-
     validation_label.config(text="")
     
     return True
 
-def on_confirm():
-    if validate_sizes():
-        # do something with the box sizes here
+def on_confirm(debug=False):
+    if validate_sizes(debug):
         print("Box sizes confirmed")
 
-def ui():
+def ui(debug=False):
+  """
+  Create the UI to ask for the dimensions of the pallet and the boxes
+  """
+
   global root
   root = tk.Tk()
   root.title("Box Size Input")
@@ -323,7 +324,7 @@ def ui():
   big_box_label.pack()
 
   global big_box_width_label
-  big_box_width_label = tk.Label(root, text="Longueur:")
+  big_box_width_label = tk.Label(root, text="Lenght:")
   big_box_width_label.pack()
 
   global big_box_width_entry
@@ -331,7 +332,7 @@ def ui():
   big_box_width_entry.pack()
 
   global big_box_depth_label
-  big_box_depth_label = tk.Label(root, text="Largeur:")
+  big_box_depth_label = tk.Label(root, text="Width:")
   big_box_depth_label.pack()
 
   global big_box_depth_entry
@@ -339,7 +340,7 @@ def ui():
   big_box_depth_entry.pack()
 
   global big_box_height_label
-  big_box_height_label = tk.Label(root, text="Hauteur:")
+  big_box_height_label = tk.Label(root, text="Height:")
   big_box_height_label.pack()
 
   global big_box_height_entry
@@ -347,11 +348,11 @@ def ui():
   big_box_height_entry.pack()
 
   global small_box_label
-  small_box_label = tk.Label(root, text="Présentoir", font="Verdana 11 underline")
+  small_box_label = tk.Label(root, text="Box", font="Verdana 11 underline")
   small_box_label.pack()
 
   global small_box_width_label
-  small_box_width_label = tk.Label(root, text="Longueur:")
+  small_box_width_label = tk.Label(root, text="Lenght:")
   small_box_width_label.pack()
 
   global small_box_width_entry
@@ -359,7 +360,7 @@ def ui():
   small_box_width_entry.pack()
 
   global small_box_depth_label
-  small_box_depth_label = tk.Label(root, text="Largeur:")
+  small_box_depth_label = tk.Label(root, text="Width:")
   small_box_depth_label.pack()
 
   global small_box_depth_entry
@@ -367,7 +368,7 @@ def ui():
   small_box_depth_entry.pack()
 
   global small_box_height_label
-  small_box_height_label = tk.Label(root, text="Hauteur:")
+  small_box_height_label = tk.Label(root, text="Height:")
   small_box_height_label.pack()
 
   global small_box_height_entry
@@ -375,7 +376,7 @@ def ui():
   small_box_height_entry.pack()
 
   global submit_button
-  submit_button = tk.Button(root, text="Confirmer", command=on_confirm)
+  submit_button = tk.Button(root, text="Confirm", command=lambda:on_confirm(debug))
   submit_button.pack()
 
   global result_label
@@ -388,17 +389,17 @@ def ui():
 
   root.mainloop()
 
-def main(log_file):
-  ui()
+def main(log_file, debug=False):
+  ui(debug)
 
-def test_one_config(carton, box, nb_boites_theorique, n, print=False):
-  if (print): printf(log_file, "\n"*3+"+-"*20+"\n"*3)
-  best_pallet = fill_pallet(carton, box, _print=print)
+def test_one_config(pallet, box, nb_boites_theorique, n, debug=False, log_file=".log"):
+  if (debug): printf(log_file, "\n"*3+"+-"*20+"\n"*3)
+  best_pallet = fill_pallet(pallet, box, debug, log_file)
   best_pallet.print_way_fill(True)
   if best_pallet.nb_box == nb_boites_theorique:
-    printf(log_file, u"\u2705" + f" TEST {n} PASSED with {100*best_pallet.nb_box*box.get_volume()//carton.get_volume()} % of the volume filled\n")
+    printf(log_file, u"\u2705" + f" TEST {n} PASSED with {100*best_pallet.nb_box*box.get_volume()//pallet.get_volume()} % of the volume filled\n")
   else:
-    printf(log_file, u"\u274C" + f" TEST {n} FAILED with {100*best_pallet.nb_box*box.get_volume()//carton.get_volume()} % of the volume filled : nb_box={best_pallet.nb_box}\n"+best_pallet.print_way_fill())
+    printf(log_file, u"\u274C" + f" TEST {n} FAILED with {100*best_pallet.nb_box*box.get_volume()//pallet.get_volume()} % of the volume filled : nb_box={best_pallet.nb_box}\n"+best_pallet.print_way_fill())
 
 def num_test():
     if hasattr(num_test, "num"):
@@ -407,21 +408,21 @@ def num_test():
         num_test.num = 1 # initialize on first call
     return num_test.num
 
-def test_all(_print=False):
+def test_all(debug=False):
   # This tests are verified by a human.
 
   printf(log_file, "*"*20+"\n TESTS \n"+"*"*20)
   
-  test_one_config(Pallet(300, 235, 177), Box(176, 106, 53), 10, num_test(), _print)
-  test_one_config(Pallet(365, 315, 220), Box(180, 139, 65), 13, num_test(), _print)
-  test_one_config(Pallet(500, 400, 300), Box(400, 300, 100), 5, num_test(), _print)
-  test_one_config(Pallet(500, 400, 400), Box(400, 300, 100), 6, num_test(), _print)
-  test_one_config(Pallet(600, 400, 400), Box(400, 300, 100), 8, num_test(), _print)
-  test_one_config(Pallet(310, 235, 250), Box(180, 139, 65), 8, num_test(), _print)
+  test_one_config(Pallet(300, 235, 177), Box(176, 106, 53), 10, num_test(), debug, log_file)
+  test_one_config(Pallet(365, 315, 220), Box(180, 139, 65), 13, num_test(), debug, log_file)
+  test_one_config(Pallet(500, 400, 300), Box(400, 300, 100), 5, num_test(), debug, log_file)
+  test_one_config(Pallet(500, 400, 400), Box(400, 300, 100), 6, num_test(), debug, log_file)
+  test_one_config(Pallet(600, 400, 400), Box(400, 300, 100), 8, num_test(), debug, log_file)
+  test_one_config(Pallet(310, 235, 250), Box(180, 139, 65), 8, num_test(), debug, log_file)
 
 if __name__ == "__main__":
   log_file = 'fill_box_optimiser.log'
-  printf_clear(log_file, False)
-  test_all(_print=False)
-  # main(log_file)
+  printf_clear(log_file)
+  # test_all(debug=False)
+  main(log_file, debug=True)
  
